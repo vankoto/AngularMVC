7d2a62a9baa6bd0264cf8afca6e3b122e81c9a47
Ivan Ivanov <ivanvelichkov357@gmail.com>
2017-06-10 16:32:00 +0200

    Manage to get response from the API

--- SinglePageApp/SinglePageApp/Scripts/myapp.js
+++ SinglePageApp/SinglePageApp/Scripts/myapp.js
@@ -1,5 +1,8 @@
-﻿var app = angular.module("myapp", []);
+﻿var app = angular.module('myapp', []);
 
-app.controller('MainController', ['$scope', function ($scope) {
-    $scope.message = "Hello AngularJS with .NET MVC";
-}]);
+app.controller('MainController', function($scope, $http) {
+    $http.get('http://swapi.co/api/people/1/').
+        then(function (response) {
+            $scope.person = response.data;
+        });
+});
