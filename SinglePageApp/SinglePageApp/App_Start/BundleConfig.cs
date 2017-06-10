6480c9bb7f8c16e6c2ed63e6a70b957b66a95364
Ivan Ivanov <ivanvelichkov357@gmail.com>
2017-06-10 15:03:58 +0200

    Add the angular.js and angular-route.js to BundleConfig

--- SinglePageApp/SinglePageApp/App_Start/BundleConfig.cs
+++ SinglePageApp/SinglePageApp/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace SinglePageApp
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js", 
+                        "~/Scripts/angular-route.js"));
+      
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
